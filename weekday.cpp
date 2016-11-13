5787ad9097fc5f6ee11677533dffac76eb30898a
Hemera7437 <oydin.iqbal@gmx.de>
2016-11-13 14:22:26 +0100

    Add files via upload

--- weekday.cpp
+++ weekday.cpp
@@ -39,7 +39,7 @@ int weekday2001(int d, int m, int y){
 							: (w == 6)? Tag = "Sonntag"
 								: Tag = "Fehler";
 	
-	std::cout << to_string(d) << "." << to_string(m) << "." << to_string(y)  << " war ein " << Tag;
+	std::cout << to_string(d) << "." << to_string(m) << "." << to_string(y)  << " war ein " << Tag << " \n";
 }
 
 int floorDiv(int a, int b){
@@ -49,6 +49,8 @@ int floorDiv(int a, int b){
 		: r = ( -( std::abs(a)  / std::abs(b)) + -1 ); 
 	return r;
 }
+/* bei - (|a| + c) / |b| muss c in Abhängigkeit von a und b eigentlich 
+c=a%b, wobei floorMod(a,b) verwendet wird, sein*/
 
 int floorMod(int a, int b) {
 	int r;
@@ -58,6 +60,35 @@ int floorMod(int a, int b) {
 	
 */
 
+int weekday(int d, int m, int y){
+	int z, j1, p, schalt, w;
+	std::string Tag;
+	
+	z = y - 2001;
+	j1 = floorMod( ( 365*z + floorDiv(z,4) - floorDiv(z,100) + floorDiv(z,400) ) , 7 ) ;
+	
+	( ( (y%4) == 0 && (y%100) != 0 ) || (y%400) == 0 ) ? schalt = 1 
+		:  schalt =0;	
+	
+	(m == 1)? p = d  : 
+		(m == 2)? p = d + 31 : 
+			(m > 2 && schalt == 0)? p = d + 59 + floorDiv((153*m - 457) , 5) : 
+				(m > 2 && schalt == 1)? p = d + 60 + floorDiv((153*m - 457) , 5) :
+					m = m;
+	
+	w = floorMod ( (j1 + p - 1) , 7);
+	
+	(w == 0)? Tag = "Montag" 
+		: (w == 1)? Tag = "Dienstag"
+			: (w == 2)? Tag = "Mittwoch"
+				: (w == 3)? Tag = "Donnerstag"
+					: (w == 4)? Tag = "Freitag"
+						: (w == 5)? Tag = "Samstag"
+							: (w == 6)? Tag = "Sonntag"
+								: Tag = "Fehler";
+	
+	std::cout << "Durch floor-Rechnung: " << to_string(d) << "." << to_string(m) << "." << to_string(y)  << " war ein " << Tag << " \n";
+}
 
 int main () {
 	int d, m, y;
@@ -68,5 +99,6 @@ int main () {
 	std::cout << "Geben sie die Jahreszahl ein" << std::endl;
 	std::cin >> y;
 	//assert(y > 1583);
-	weekday2001(d, m, y);	
+	weekday2001(d, m, y);
+	weekday(d, m, y);
 }
