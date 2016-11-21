c043aad5c8169f4a2e60156f2b91104091f4e311
Hemera7437 <oydin.iqbal@gmx.de>
2016-11-21 17:01:26 +0100

    Add files via upload

---
+++ Zettel4/pythagoras.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+#include <cassert>
+
+using namespace std;
+
+bool is_square(int n){
+	double x, y;
+	
+	x = sqrt(n); //Aufgabe 2a
+	y = floor(x);
+	
+	if (n%4 ==2 || n%4 ==3){  //Aufgabe 2b
+		return false;
+	}
+	
+	if (x == y){
+		return true;
+	}
+	else {
+		return false;
+	}	
+}
+
+void pythagorean_triple(int bmax){
+	int a, b;
+	for (b = 1; b <= bmax; ++b){
+		for (a = 1; a < b; ++a){
+			if ( is_square(a*a + b*b) == true ) {
+				cout << "(" << a << ", " << b ", " << c << ")";
+			}
+		}
+	}
+}
+
+
+int main (){
+	int bmax = 400;
+	pythagorean_triple(bmax);	
+}
