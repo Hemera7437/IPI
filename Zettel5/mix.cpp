781cd511e86bb44def6f9aef9bd473427d309cb3
Hemera7437 <oydin.iqbal@gmx.de>
2016-11-26 19:02:43 +0100

    Add files via upload

---
+++ Zettel5/mix.cpp
@@ -0,0 +1,15 @@
+#include <iostream>	
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+vector <std::string> split_words(std::string s)
+{
+    s.find(" ");
+}
+
+int main () {
+    return 0;
+}
