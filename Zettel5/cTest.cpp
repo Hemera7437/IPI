1bde6f4a945d86cb1a7573e5ef3fb6d39c093f37
Hemera7437 <oydin.iqbal@gmx.de>
2016-11-27 13:03:39 +0100

    Add files via upload

--- Zettel5/cTest.cpp
+++ Zettel5/cTest.cpp
@@ -5,46 +5,51 @@
 
 using namespace std;
 
-vector <string> findeLeerzeichen(string s){
-    vector<string> Leerzeichen;
+vector <string> findSpace(string s){
+    vector<string> space;
     
     int length = s.size();
-    int pos = s.find(" ");
+    int pos = s.find(' ');
     int i;     
 	string pickName;
     for(int i = 0; i < length; ++i){
-		if(s[i] == pos){
-			Leerzeichen.push_back(pickName);
+		if(s[i] == ' '){
+			space.push_back(pickName);			// sollte ein Leerzeichen vorkommen, wird das wort in den vector eingefügt
+			pickName = "";								// danach wird der string wieder resettet um keine doppelten zeichen zu übertragen
 		}else{
-			pickName =+ s[i];
+			pickName += s[i];							// sollte der character kein Leerzeichen sein wird er dem hilfs string angefügt
 		}
 	}
-    
-    return Leerzeichen;
+	space.push_back(pickName);					// hier wird das letzte wort auch noch hinzu gefügt, weil normalerweise der letzte character kein Leerzeichen ist
+    return space;
 }
 
-vector <string> split_words(string s)
+vector <string> split_words(string s)					// diese funktion wurde übertragen aus dem was ich ursprünglich gezeigt bekommen habe
 {
     vector<string> v1;    
-    v1.push_back(s);
     
-    //s.find(" ");
+    vector<string> c = findSpace(s);
+    
+	for(int i = 0; i < c.size(); ++i){					// hier wird jedes wort einzeln ausgespuckt
+		v1.push_back(c[i]);
+        //cout << c[i] << endl;
+	}
     
     return v1;
 }
 
 int main () {
-    string s = "Alpha Beta! Gamma";
-    string s1, s2;
+    string s;
+	cout << "Please enter a sentence" << endl;
+	getline(cin, s);
     
-    vector<string> l = findeLeerzeichen(s);
-    
-	for(int i = 0; i < l.size(); ++i){
-		cout << l[i] << " , " << endl;
-	}    
-    
+    vector<string> c = findSpace(s);
 	vector<string> b = split_words(s);
     
+	for(int i = 0; i < c.size(); ++i){					// hier wird jedes wort einzeln ausgespuckt
+		cout << c[i] << endl;
+	}
+        
 	for(int i = 0; i < b.size(); ++i){
 		cout << b[i] << endl;
 	}
